9bc9b936bd50045442c03596c2189826876bfe4a
Raj Panchal <35273390+rajpanchal@users.noreply.github.com>
2018-09-06 20:22:05 +0530

    add comments for better understanding to review of peers

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,19 +4,19 @@ function guess(){
     true_ans=$(ls -l |grep "^-"|wc -l)
     while true;
     do
-        echo "Enter your number of files in the directory - JUST GUESS :)"
+        echo "Enter your number of files in the directory - JUST GUESS :)" #ENTER THE GUESS
         read  number
-        if [ $number -lt $true_ans ]
+        if [ $number -lt $true_ans ]  #CHECKING IF LESS
         then
-            echo "Your guess is less than number of files"
-        elif [ $number -gt $true_ans ]
+            echo "Your guess is less than number of files" #ACKNOWLEDGING IF LESS
+        elif [ $number -gt $true_ans ] #CHECKING IF GREATER
         then
-            echo "Your guess is greater than number of files"
+            echo "Your guess is greater than number of files" #ACKNOWLEDGING IF GREATER
         else
-            echo "Congratulations, you guessed it right!"
+            echo "Congratulations, you guessed it right!" #CONGRATS FOR PASSING
         break;
         fi
     done
 }
-echo "Guess total number of files in this directory,"
+echo "Guess total number of files in this directory," #INITIAL OUTPUT FOR GUSEEING GAME
 guess
